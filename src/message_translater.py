714590b74d297439f87238a924384297d13154c9
wchistow <wchistow@yandex.ru>
2022-08-03 11:34:27 +0300

    add error message and type handling to message_translater.py

--- src/message_translater.py
+++ src/message_translater.py
@@ -1,15 +1,25 @@
 def translate_message(err_message: list[str]) -> str:
-    """Возвращает русский текст объяснения ошибки."""
+    """Возвращает русский текст ошибки."""
     err_message = err_message[1:]
-
-    result = f'{_get_location(err_message[0]).capitalize()}:\n  {err_message[1]}'
-
+    result = f'{_get_location(err_message[0]).capitalize()}:\n  {err_message[1]}\n{_get_message(err_message[2])}'
     return result
 
 
 def _get_location(location: str) -> str:
-    """Возвращает русский текст объяснения места ошибки."""
+    """Возвращает русский текст места ошибки."""
     file = location[6:location.rfind('"')]
     line = location[location.find(',') + 7: location.rfind(',')]
-
     return f'в файле "{file}", строке {line}'
+
+
+def _get_message(message: str) -> str:
+    """Возвращает русский текст сообщения ошибки."""
+    err_type = message[:message.find(':')]
+    return f'{_get_error_type(err_type).capitalize()}: '
+
+
+def _get_error_type(err_type: str) -> str:
+    """Возвращает русский перевод типа ошибки."""
+    match err_type:
+        case 'NameError':
+            return 'ошибка имени'
