c885a4381faad60a54fd6b8c1138fade1bc53757
wchistow <wchistow@yandex.ru>
2022-08-04 12:04:20 +0300

    add function _get_message to file message_translater.py

--- src/message_translater.py
+++ src/message_translater.py
@@ -1,7 +1,10 @@
+import re
+
+
 def translate_message(err_message: list[str]) -> str:
     """Возвращает русский текст ошибки."""
     err_message = err_message[1:] if err_message[0] == 'Traceback (most recent call last):' else err_message
-    result = f'{_get_location(err_message[0]).capitalize()}:\n  {err_message[1]}\n{_get_message(err_message[2])}'
+    result = f'{_get_location(err_message[0]).capitalize()}:\n  {err_message[1]}\n{_get_error(err_message[2])}'
     return result
 
 
@@ -12,10 +15,11 @@ def _get_location(location: str) -> str:
     return f'в файле "{file}", строке {line}'
 
 
-def _get_message(message: str) -> str:
-    """Возвращает русский текст сообщения ошибки."""
+def _get_error(message: str) -> str:
+    """Возвращает русский текст названия и сообщения ошибки."""
     err_type = message[:message.find(':')]
-    return f'{_get_error_type(err_type).capitalize()}: '
+    err_message = message[message.find(':') + 2:]
+    return f'{_get_error_type(err_type).capitalize()}: {_get_message(err_type, err_message)}.'
 
 
 def _get_error_type(err_type: str) -> str:
@@ -23,4 +27,14 @@ def _get_error_type(err_type: str) -> str:
     match err_type:
         case 'NameError':
             return 'ошибка имени'
+    return 'неизвестная ошибка'
+
+
+def _get_message(err_type: str, message: str) -> str:
+    """Возвращает русский текст сообщения ошибки."""
+    match err_type:
+        case 'NameError':
+            if re.match(r"name '\w+' is not defined.", message) is not None:
+                name = message[6: message.rfind("'")]
+                return f"имя '{name}' не определено"
     return 'неизвестная ошибка'
