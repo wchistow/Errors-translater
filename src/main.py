b84340cbbf66b5df6beb3fe6514109ed438ce10e
wchistow <wchistow@yandex.ru>
2022-08-09 10:59:13 +0300

    change regular expressions

--- src/main.py
+++ src/main.py
@@ -8,14 +8,14 @@ count = 4
 while count != 0:
     line = input().strip()
 
-    if count == 1 and re.match(r'[A-Z]\w+: .+', line) is None:  # Должна быть последняя строка, но это не она.
+    if count == 1 and re.match(r'^[A-Z]\w+: .+$', line) is None:  # Должна быть последняя строка, но это не она.
         err_message.append(line)
-        while re.match(r'[A-Z]\w+: .+', line) is None:
+        while re.match(r'^[A-Z]\w+: .+$', line) is None:
             line = input().strip()
             err_message.append(line)
         break
 
-    if re.match(r'File "<stdin>", line \d+, in <module>', line) is not None:
+    if re.match(r'^File "<stdin>", line \d+, in <module>$', line) is not None:
         count -= 1
     err_message.append(line)
 
--- src/message_translater.py
+++ src/message_translater.py
@@ -10,7 +10,7 @@ def translate_message(err_message: list[str]) -> str:
     err_message = err_message[1:] if err_message[0] == 'Traceback (most recent call last):' else err_message
 
     if len(err_message) in (2, 3):
-        line = '' if re.match(r'File "<stdin>", line \d+, in <module>', err_message[0]) is not None else err_message[1]
+        line = '' if re.match(r'^File "<stdin>", line \d+, in <module>$', err_message[0]) is not None else err_message[1]
 
         location = _get_location(err_message[0])
         message = _get_error(err_message[1] if line == '' else err_message[2])
@@ -24,9 +24,9 @@ def translate_message(err_message: list[str]) -> str:
         result = ''
 
         for err_line in err_message:
-            if re.match(r'File ".+", line \d+, in .+', err_line) is not None:
+            if re.match(r'^File ".+", line \d+, in .+$', err_line) is not None:
                 result += f'{_get_location(err_line).capitalize()}:\n  '
-            elif re.match(r'[A-Z]\w+: .+', err_line) is not None:
+            elif re.match(r'^[A-Z]\w+: .+$', err_line) is not None:
                 result += _get_error(err_line)
                 break
             else:  # Строка кода
@@ -39,7 +39,7 @@ def translate_message(err_message: list[str]) -> str:
 
 def _get_location(location: str) -> str:
     """Возвращает русский текст места ошибки."""
-    if re.match(r'File "<stdin>", line \d+, in <module>', location):
+    if re.match(r'^File "<stdin>", line \d+, in <module>$', location):
         return 'в интерпретаторе'
     else:
         file = location[6:location.rfind('"')]
@@ -76,11 +76,11 @@ def _get_message(err_type: str, message: str) -> str:
     """Возвращает русский текст сообщения ошибки."""
     match err_type:
         case 'NameError':
-            if re.match(r"name '\w+' is not defined", message) is not None:
+            if re.match(r"^name '\w+' is not defined$", message) is not None:
                 name = message[6: message.rfind("'")]
                 return f"имя '{name}' не определено"
         case 'TypeError':
-            if re.match(r"unsupported operand type\(s\) for [+\-*/|&^]: '\w+' and '\w+'", message) is not None:
+            if re.match(r"^unsupported operand type\(s\) for [+\-*/|&^]: '\w+' and '\w+'$", message) is not None:
                 operator = message[32: 33]
                 operand1 = message[message.find("'") + 1: message.find("'", message.find("'") + 1)]
                 operand2 = message[47: message.rfind("'")]
