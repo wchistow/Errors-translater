7607fc03b844d223462f032520d6987577c94e2d
wchistow <wchistow@yandex.ru>
2022-10-21 14:29:06 +0300

    add db errors_messages.json

--- src/message_translater.py
+++ src/message_translater.py
@@ -100,51 +100,52 @@ def _get_error_type(err_type: str) -> str:
 
 def _get_message(err_type: str, message: str) -> str:
     """Возвращает русский текст сообщения ошибки."""
+    errors_messages = json.load(open('db/errors_messages.json', encoding='utf-8'))
+
     if message == 'None':
         return ''
 
-    match err_type:
-        case 'NameError':
-            if re.match(r"^name '\w+' is not defined[.| ]$", message) is not None:
-                name = message[6: message.rfind("'")]
-                return f"имя '{name}' не определено"
-            elif re.match(r"^name '\w+' is not defined. Did you mean: '\w+'\?$", message) is not None:
-                name = ''
-                in_name = False
-                for s in message:
-                    if s == "'" and name == '':
-                        in_name = True
-                    elif s == "'" and name != '':
-                        in_name = False
-                    elif in_name:
-                        name += s
-                mean_name = ''
-                in_mean_name = False
-                for s in message[message.find('.'):]:
-                    if s == "'" and mean_name == '':
-                        in_mean_name = True
-                    elif s == "'" and mean_name != '':
-                        in_mean_name = False
-                    elif in_mean_name:
-                        mean_name += s
-                return f"имя '{name}' не определено. Может быть вы имели ввиду '{mean_name}'?"
-        case 'TypeError':
-            if re.match(r"unsupported operand type\(s\) for [+\-*/|&^]: '\w+' and '\w+'", message) is not None:
-                operator = message[32: 33]
-                operand1 = message[message.find("'") + 1: message.find("'", message.find("'") + 1)]
-                operand2 = message[47: message.rfind("'")]
-                return f"неподдерживаемый оператор {operator} для типов '{operand1}' и '{operand2}'"
-        case 'SyntaxError':
-            if message == 'invalid syntax':
-                return 'неправильно написана строка'
-            elif message == "'return' outside function":
-                return "ключевое слово 'return' не в функции"
-            elif message == "'break' outside loop":
-                return "ключевое слово 'break' не в цикле"
-            elif message == "'continue' not properly in loop":
-                return "ключевое слово 'continue' не в цикле"
-        case 'ZeroDivisionError':
-            return 'на ноль делить нельзя'
-        case 'FileNotFoundError':
-            return f'{message[38:-1]}'
-    return message
+    try:
+        messages = errors_messages[err_type]
+        if isinstance(messages, dict):
+            try:
+                return messages[message]
+            except KeyError:
+                return 'неизвестная ошибка'
+        else:
+            return messages
+    except KeyError:
+        match err_type:
+            case 'NameError':
+                if re.match(r"^name '\w+' is not defined[.| ]$", message) is not None:
+                    name = message[6: message.rfind("'")]
+                    return f"имя '{name}' не определено"
+                elif re.match(r"^name '\w+' is not defined. Did you mean: '\w+'\?$", message) is not None:
+                    name = ''
+                    in_name = False
+                    for s in message:
+                        if s == "'" and name == '':
+                            in_name = True
+                        elif s == "'" and name != '':
+                            in_name = False
+                        elif in_name:
+                            name += s
+                    mean_name = ''
+                    in_mean_name = False
+                    for s in message[message.find('.'):]:
+                        if s == "'" and mean_name == '':
+                            in_mean_name = True
+                        elif s == "'" and mean_name != '':
+                            in_mean_name = False
+                        elif in_mean_name:
+                            mean_name += s
+                    return f"имя '{name}' не определено. Может быть вы имели ввиду '{mean_name}'?"
+            case 'TypeError':
+                if re.match(r"unsupported operand type\(s\) for [+\-*/|&^]: '\w+' and '\w+'", message) is not None:
+                    operator = message[32: 33]
+                    operand1 = message[message.find("'") + 1: message.find("'", message.find("'") + 1)]
+                    operand2 = message[47: message.rfind("'")]
+                    return f"неподдерживаемый оператор {operator} для типов '{operand1}' и '{operand2}'"
+            case 'FileNotFoundError':
+                return f'{message[38:-1]}'
+        return 'неизвестная ошибка'
